b01d3915bf5d6498d772540cd3aa20dedaf6172c
Febrina Anissa <febrinaanissa@gmail.com>
2017-04-01 09:28:28 +0700

    commit kelima

---
+++ src/posttest1_1/konversisuhu.java
@@ -0,0 +1,21 @@
+package posttest1_1;
+
+
+import posttest1_1.Frame_program_konversi_suhu;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Glbertzy
+ */
+public class konversisuhu {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Frame_program_konversi_suhu Laund = new Frame_program_konversi_suhu();
+        Laund.setVisible(true);
+    }
+}
